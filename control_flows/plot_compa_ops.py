11234e3a0a760be321bc7ca3a1d9581092878aa9
Vincent Roulet <vroulet@google.com>
2024-06-05 17:26:59 -0700

    add copyrights

--- control_flows/plot_compa_ops.py
+++ control_flows/plot_compa_ops.py
@@ -1,3 +1,16 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -64,5 +77,7 @@ def plot_compa_ops(type, fig_title=None):
 
 
 plot_compa_ops('hard')
+plot_compa_ops('soft_logistic', 'Smoothed operators with logistic')
+
 
 plt.show()
